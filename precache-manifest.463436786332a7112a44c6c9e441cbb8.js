f2dbe911c170c8830012e88d989959a9bacee87c
Youngjae <yj122198@naver.com>
2020-10-02 16:28:16 +0900

    Updates

---
+++ precache-manifest.463436786332a7112a44c6c9e441cbb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f568a830f4e1883539c341f0d27a8d89",
+    "url": "/test_publish/index.html"
+  },
+  {
+    "revision": "91b6e684dbdbf9bcc259",
+    "url": "/test_publish/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9fc71bae6436764bd921",
+    "url": "/test_publish/static/js/2.f31891d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_publish/static/js/2.f31891d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b6e684dbdbf9bcc259",
+    "url": "/test_publish/static/js/main.14855a39.chunk.js"
+  },
+  {
+    "revision": "a8d5b8f1c3e620057158",
+    "url": "/test_publish/static/js/runtime-main.722cee88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_publish/static/media/logo.5d5d9eef.svg"
+  }
+]);
