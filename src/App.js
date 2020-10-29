398bd7c80328adb7e893f76867c45e379a5f73af
Youngjae Kim <yj122198@naver.com>
2020-10-29 20:19:19 +0900

    learn state to effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading..");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div>
+      <div>Hi</div>
+    </div>
+  );
+};
 
 export default App;
+
+/**useState 함수
+  const [item, setItem] = useState(0);
+  return (
+    <div>
+      <p>Hello {item}</p>
+      <button onClick={() => setItem(item + 1)}>increase</button>
+      <button onClick={() => setItem(item - 1)}>decrease</button>
+    </div>
+  );
+ */
+/** useInput 함수
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let update = true;
+    if (typeof validator === "function") {
+      console.log(value);
+      update = validator(value);
+    }
+    if (update) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div>
+      <h1>hello</h1>
+      <input {...name} />
+    </div>
+  );
+};
+ */
+
+/** useTabs 함수
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+};
+  */
